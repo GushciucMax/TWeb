e854f1e0afc44a671a8f739a0b180d30a826089e
GushciucMax <gushciucmaxim@gmail.com>
2021-03-18 20:19:14 +0200

    18.03.2021 layout finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'antd/dist/antd.css';
 import './App.css';
+import { Layout } from 'antd';
+import { HeaderLayout } from './Layouts/HeaderLayout/HeaderLayout';
+import { ContentLayout } from './Layouts/ContentLayout/ContentLayout';
+import { FooterLayout } from './Layouts/FooterLayout/FooterLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Layout>
+			<HeaderLayout />
+            <ContentLayout />
+			<FooterLayout />
+        </Layout>
+    </>
   );
 }
 
