b306914103fa7056fcef16bc5b96e45d22232504
GushciucMax <gushciucmaxim@gmail.com>
2021-04-21 18:40:49 +0300

    API get

--- src/mobx/ProviderRootStore/ProviderRootStore.tsx
+++ src/mobx/ProviderRootStore/ProviderRootStore.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { RootStore$ } from "../stores/RootStore$";
+import { IRoot$ } from "../interfaces";
 
 const MainStore = RootStore$.create({})
 
-const StoreContext = React.createContext({})
+const StoreContext = React.createContext<IRoot$ | null>(null)
 
 export const StoreProvider = (props) => {
     return <StoreContext.Provider value={ MainStore }> { props.children } </StoreContext.Provider>
 }
 
-export function useRootStore() {
+export function useRootStore(): IRoot$ {
     const store = React.useContext(StoreContext)
     if (!store) {
         throw new Error('useStore shall be used within StoreProvider')
